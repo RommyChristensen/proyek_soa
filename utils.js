3e472d5757ee2d6a7abca4c0e9694b0fe05c5850
Rommy Christensen <rommycy00@gmail.com>
2021-05-08 21:40:02 +0700

    add utils

---
+++ utils.js
@@ -0,0 +1,27 @@
+const { pool } = require('./connection');
+
+function getConnection() {
+    return new Promise(function (resolve, reject) {
+        pool.getConnection(function (err, connection) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(connection);
+            }
+        });
+    });
+}
+
+function executeQuery(conn, query) {
+    return new Promise(function (resolve, reject) {
+        conn.query(query, function (err, result) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(result);
+            }
+        });
+    });
+}
+
+module.exports = {getConnection, executeQuery};
