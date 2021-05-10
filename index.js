d2c16216ada77f8214ac52019115e6f72bd88f07
Monica Chandra <monchan250101.mc@gmail.com>
2021-05-10 09:17:51 +0700

    ADMIN - PLAN

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const utils = require("./helpers/utils");
 const auth = require("./routes/user/auth");
 const admin_kategori = require("./routes/admin/kategori");
 const admin_hashtag = require("./routes/admin/hashtag");
+const admin_plan = require("./routes/admin/plan");
 const app = new express();
 
 app.use(express.urlencoded({extended: true}));
@@ -10,6 +11,7 @@ app.use(express.urlencoded({extended: true}));
 // ROUTES
 app.use("/api/admin/kategori", admin_kategori);
 app.use("/api/admin/hashtag", admin_hashtag);
+app.use("/api/admin/plan", admin_plan);
 app.use("/api/user", auth);
 
 
---
+++ routes/admin/plan.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const plan_model = require('../../models/admin/plans');
+const { ifExists } = require("../../helpers/utils");
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    const result = await plan_model.getData();
+    return res.status(200).send(result);
+});
+
+router.get('/:plan_id', async (req, res) => {
+    if(! await ifExists("plans", "plan_id", req.params.plan_id)){
+        return res.status(404).send({error: "plan not found"});
+    }
+    const result = await plan_model.getDataById(req.params.plan_id);
+    return res.status(200).send(result[0]);
+});
+
+router.post('/', async (req, res) => {
+    const nama = req.body.nama;
+    const deskripsi = req.body.deskripsi;
+    const harga = parseInt(req.body.harga);
+    const data = {
+        plan_nama : nama,
+        plan_deskripsi : deskripsi,
+        plan_harga : harga
+    };
+    if(await ifExists("plans", "plan_nama", nama)){
+        return res.status(400).send({error: "plan has been registered"});
+    }
+    const plan_id=await plan_model.insertData(data);
+    if(plan_id!=null){
+        let datareturn = {
+            plan_id : plan_id,
+            plan_nama : nama,
+            plan_deskripsi : deskripsi,
+            plan_harga : harga
+        };
+        return res.status(201).send(datareturn);
+    }else{
+        return res.status(500).send({error: "Something went wrong"});
+    }
+});
+
+router.put('/:plan_id', async (req, res) => {
+    const { plan_id } = req.params;
+    const deskripsi = req.body.deskripsi;
+    const harga = parseInt(req.body.harga);
+    if(! await ifExists("plans", "plan_id", plan_id)){
+        return res.status(404).send({error: "plan not found"});
+    }
+    const result = await plan_model.updateData(plan_id, deskripsi,harga);
+    return res.status(200).send(result);
+});
+
+module.exports = router;
